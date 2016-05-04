9ff64ce27de740e0548bf4ca56c2a3b374478101
Chris Brundage <chris.brundage@thechive.com>
2016-05-04 16:11:52 -0500

    Fixed the is_master fact to only run on MongoDB servers.

--- lib/facter/is_master.rb
+++ lib/facter/is_master.rb
@@ -2,7 +2,7 @@ require 'json';
 
 Facter.add('mongodb_is_master') do
   setcode do
-    if Facter::Core::Execution.which('mongo') 
+    if Facter::Core::Execution.which('mongod') 
       if File.file?('/root/.mongorc.js')
         mongo_output = Facter::Core::Execution.exec('mongo --quiet --eval "load(\'/root/.mongorc.js\'); printjson(db.isMaster())"')
       else
